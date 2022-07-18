361e6a25dd3ea325c2e2ac02b5103f7a3840ccfa
Bruno Alves <vespidhook@gmail.com>
2022-07-18 20:03:34 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { NativeBaseProvider, Box } from "native-base";
+
+import { SignIn } from "./src/screens/SignIn";
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <SignIn />
+    </NativeBaseProvider>
+  );
+}
