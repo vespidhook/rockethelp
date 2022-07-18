1dcedda9655d8c48e7a6e664c65b7341e5f07006
Bruno Alves <vespidhook@gmail.com>
2022-07-18 20:40:21 -0300

    statusbar

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,7 @@
 import { Heading, VStack } from 'native-base';
 
+import Logo from '../assets/logo_primary.svg';
+
 export function SignIn () {
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
