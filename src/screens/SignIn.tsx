7cbe92ad7df0cdea7379731fb78761414348e039
Bruno Alves <vespidhook@gmail.com>
2022-07-18 22:35:35 -0300

    icons

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,13 +1,25 @@
-import { Heading, VStack } from 'native-base';
+import { Heading, VStack, Icon } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
 
 import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/Input';
 
 export function SignIn () {
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo />
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
             </Heading>
+
+            <Input 
+                placeholder="E-mail"
+                mb={4}
+                InputLeftElement={<Icon as={<Envelope />} /> }
+            />
+            <Input 
+                placeholder="Senha"
+            />
         </VStack>
     )
 }
