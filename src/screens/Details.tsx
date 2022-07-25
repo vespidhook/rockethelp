e5d5e64b348b85c26999900dd52f13a4152d1fc5
Bruno Alves <vespidhook@gmail.com>
2022-07-25 19:19:53 -0300

    login

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,22 +1,75 @@
-import { VStack, Text as NativeBaseText } from 'native-base';
+import { useEffect, useState } from 'react';
+import { VStack, Text as NativeBaseText, HStack, useTheme } from 'native-base';
 import { useRoute } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
+
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
+import { dateFormat } from '../utils/firestoreFormatDate';
 
 import { Header } from '../components/Header';
+import { OrderProps } from '../components/Order';
+import { Loading } from '../components/Loading';
+import { CircleWavyCheck, Hourglass } from 'phosphor-react-native';
 
 type RouteParams = {
     orderId: string;
 }
 
+type OrderDetails = OrderProps & {
+  description: string;
+  solution: string;
+  closed: string;
+}
+
 export function Details() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [solution, setSolution] = useState<string>('');
+  const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
+
+  const { colors } = useTheme();
+
   const route = useRoute();
   const { orderId } = route.params as RouteParams;
+
+  useEffect(() => {
+    firestore()
+    .collection<OrderFirestoreDTO>('orders')
+    .doc(orderId)
+    .get()  
+    .then((doc) => {
+      const { patrimony, description, status, created_at, closed_at, solution } = doc.data();
+
+      const closed = closed_at ? dateFormat(closed_at) : null;
+
+      setOrder({
+        id: doc.id,
+        patrimony,
+        description,
+        status,
+        solution,
+        when: dateFormat(created_at),
+        closed
+      });
+
+      setIsLoading(false);
+    })
+  }, []);
+
+  if(isLoading) {
+    return <Loading />
+  }
 
   return (
     <VStack flex={1} bg="gray.700">
         <Header title="Solicitação" />
-        <NativeBaseText color="white">
-          {orderId}
-        </NativeBaseText>
+        
+        <HStack bg="gray.500" justifyContent="center" p={4}>
+          {
+            order.status === 'closed'
+            ? <CircleWavyCheck size={22} color={colors.green[300]} />
+            : <Hourglass size={22} color={colors.green[300]} />
+          }
+        </HStack>
     </VStack>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,39 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 import { useNavigation } from '@react-navigation/native';
 import { Heading, HStack, IconButton, Text as NativeBaseText, useTheme, VStack, FlatList, Center } from 'native-base';
 import { ChatTeardropText, SignOut } from 'phosphor-react-native';
+
+import { dateFormat } from '../utils/firestoreFormatDate';
 
 import Logo from '../assets/logo_secondary.svg';
 
 import { Filter } from '../components/Filter';
 import { Button } from '../components/Button';
+import { Loading } from '../components/Loading';
 import { Order, OrderProps } from '../components/Order';
+import { isLoading } from 'expo-font';
 
 export function Home() {
+    const [isLoading, setIsLoading] = useState(true);
     const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
-    const [orders, setOrders] = useState<OrderProps[]>([
-        {
-            id: '1',
-            patrimony: '123456',
-            when: '2020-01-01',
-            status: 'open',
-        },
-        {
-            id: '2',
-            patrimony: '123456',
-            when: '2020-01-01',
-            status: 'open',
-        },
-        {
-            id: '3',
-            patrimony: '123456',
-            when: '2020-01-01',
-            status: 'open',
-        },
-
-    ]);
+    const [orders, setOrders] = useState<OrderProps[]>([]);
 
     const navigation = useNavigation();
     const { colors } = useTheme();
@@ -54,6 +40,32 @@ export function Home() {
             return Alert.alert('Erro', 'Não foi possivel sair.');            
         });
     }
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        const subscriber = firestore()
+        .collection('orders')
+        .where('status', '==', statusSelected)
+        .onSnapshot(snapshot => {
+            const data = snapshot.docs.map(doc => {
+                const { patrimony, description, status, created_at } = doc.data();
+
+                return {
+                    id: doc.id,
+                    patrimony,
+                    description,
+                    status,
+                    when: dateFormat(created_at),
+                }
+            })
+
+            setOrders(data);
+            setIsLoading(false);
+        });
+
+        return subscriber;
+    }, [statusSelected]);
 
   return (
     <VStack flex={1} pb={6} bg="gray.700">
@@ -102,22 +114,25 @@ export function Home() {
                 />
             </HStack>
 
-            <FlatList 
-                data={orders}
-                keyExtractor={item => item.id}
-                renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} /> }
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 50 }}
-                ListEmptyComponent={() => (
-                    <Center>
-                        <ChatTeardropText color={colors.gray[300]} size={40} />
-                        <NativeBaseText color="gray.300" fontSize="xl" mt={6} textAlign="center">
-                            Você ainda não possui {'\n'}
-                            solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizados'}
-                        </NativeBaseText>
-                    </Center>                
-                )}
-            />
+            {
+                isLoading ? <Loading /> : 
+                <FlatList 
+                    data={orders}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} /> }
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 50 }}
+                    ListEmptyComponent={() => (
+                        <Center>
+                            <ChatTeardropText color={colors.gray[300]} size={40} />
+                            <NativeBaseText color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                                Você ainda não possui {'\n'}
+                                solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizados'}
+                            </NativeBaseText>
+                        </Center>                
+                    )}
+                />
+            }
 
             <Button title="Nova solicitação" onPress={handleNewOrder} />
         </VStack>
